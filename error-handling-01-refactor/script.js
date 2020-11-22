5aa2ca7ee401f8bec1800b9f78b4b4112a7ca003
HAZberi <mhazuberi@gmail.com>
2020-11-21 21:01:02 -0700

    final commit

--- error-handling-01-refactor/script.js
+++ error-handling-01-refactor/script.js
@@ -3,7 +3,7 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-//////////////////////// Using Catch and Finally /////////////////////
+//////////////////////// Using Catch, Finally, THROW /////////////////////
 
 const displayCountries = function (data, className = '') {
   const html = `        
@@ -30,27 +30,26 @@ const handlErr = function (err) {
   return `Something went wrong. ${err.message} Shitt Mannn 😲😲😲`;
 };
 
-const getCountryInfo = function (country) {
-  fetch(`https://restcountries.eu/rest/v2/name/${country}`)
-    .then(response => {
-      if (!response.ok)
-        throw new Error(`|${country}| NOT FOUND ${response.status}`);
-      return response.json();
-    })
-    .then(([data]) => {
+const getJson = function (url, err) {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${err} : NOT FOUND ${response.status} `);
+    return response.json();
+  });
+};
+
+/* const getCountryInfo = function (country) {
+  getJson(`https://restcountries.eu/rest/v2/name/${country}`, `${country}`).then(([data]) => {
       displayCountries(data);
       const neighbour = data.borders[0];
-      if (!(data.borders.length > 1))
+      if (!(data.borders.length <= 1))
         throw new Error(
           `${country[0].toUpperCase()}${country.slice(1)} has NO neighbours!!`
         );
       //then method always return the promises so always return from last then()
-      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
-    })
-    .then(response => {
-      if (!response.ok)
-        throw new Error(`Country NOT FOUND ${response.status}`);
-      return response.json();
+      return getJson(
+        `https://restcountries.eu/rest/v2/alpha/${neighbour}`,
+        `${neighbour}`
+      );
     })
     .then(data => {
       displayCountries(data, 'neighbour');
@@ -61,5 +60,35 @@ const getCountryInfo = function (country) {
     .finally(() => {
       countriesContainer.style.opacity = 1;
     });
+}; */
+
+/////////////////////////////////////MY Own Challenge///////////
+
+const getCountryInfo = function (country) {
+  getJson(`https://restcountries.eu/rest/v2/name/${country}`, `${country}`)
+    .then(([data]) => {
+      displayCountries(data);
+      if (!(data.borders.length >= 1))
+        throw new Error(
+          `${country[0].toUpperCase()}${country.slice(1)} has NO neighbours!!`
+        );
+      //then method always return the promises so always return from last then()
+      data.borders.forEach(neighbour => {
+        getJson(
+          `https://restcountries.eu/rest/v2/alpha/${neighbour}`,
+          `${neighbour}`
+        )
+        .then(data => {
+          displayCountries(data, 'neighbour');
+        })
+        .catch(err =>
+          countriesContainer.insertAdjacentText('beforeend', handlErr(err))
+        )
+        .finally(() => {
+          countriesContainer.style.opacity = 1;
+        });  
+      });    
+    })
 };
-btn.addEventListener('click', e => getCountryInfo('Australia'));
+
+btn.addEventListener('click', e => getCountryInfo('china'));
