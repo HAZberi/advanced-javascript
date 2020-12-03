16f780b3a3c68467fc8c09e77d519305db0d6f1d
HAZberi <mhazuberi@gmail.com>
2020-12-03 04:09:03 -0700

    first import

--- npm/script.js
+++ npm/script.js
@@ -3,6 +3,20 @@
 
 import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
 
+const state = {
+    cart : [
+        { product: 'pizza', quantity: 5},
+        { product: 'wings', quantity: 15},
+    ],
+    user: { loggedIn: true}
+}
+
+const stateClone = Object.assign({}, state);
+const stateCloneDeep = cloneDeep(state);
+console.log(stateClone);
+console.log(stateCloneDeep);
+state.user.loggedIn = false;
+console.log(stateClone);
 
 
 
