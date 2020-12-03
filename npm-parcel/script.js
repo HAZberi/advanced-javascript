954e76938dfd41bcfe547a019d38efdece5ebc19
HAZberi <mhazuberi@gmail.com>
2020-12-03 09:19:42 -0700

    final commit

--- npm-parcel/script.js
+++ npm-parcel/script.js
@@ -1,5 +1,10 @@
+//Initialize and download all the dependencies first
+//Commands
+//npm i
+//npm run start
+//then it should be good to go.
 
-"use strict"
+'use strict';
 //This is Importing Module
 //All modules are by-default executed in 'strict mode'
 //Import statement are executed asyncronously
@@ -8,48 +13,52 @@
 //Import statements are written at the start of the code in practice
 
 //Part 1, multiple named exports
-import "./shoppingCart.js";
-import {addToCart, shippingCost as shipping, amountDue as dueToday} from "./shoppingCart.js";
+import './shoppingCart.js';
+import {
+  addToCart,
+  shippingCost as shipping,
+  amountDue as dueToday,
+} from './shoppingCart.js';
 console.log('Importing Module');
 
 // //Cannot use varibles and function names from exporting modules
 // //console.log(totalCost);
 
 // //Using an imported module
-addToCart("book", 5);
+addToCart('book', 5);
 
 console.log(shipping, dueToday);
 
 //Part 2, export all top level/global scoped varibles and functions
 
-import * as ShoppingCart from "./shoppingCart.js";
+import * as ShoppingCart from './shoppingCart.js';
 
 ShoppingCart.addToCart('Pencil', 5);
 console.log(ShoppingCart.amountDue);
 
 //Part 3, export default values
 
-import add2Cart from "./shoppingCart.js";
+import add2Cart from './shoppingCart.js';
 add2Cart('shapener', 3);
 
 //Part 4 Mixing default and named export is possible but not recommended
 
-import add, {shippingCost, cart, amountDue} from "./shoppingCart.js";
+import add, { shippingCost, cart, amountDue } from './shoppingCart.js';
 
 add('pen', 10);
 console.log(shippingCost);
 console.log(amountDue);
 cart.forEach(pd => console.log(pd.item));
 
-import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
+import cloneDeep from 'lodash-es';
 
 const state = {
-    cart : [
-        { product: 'pizza', quantity: 5},
-        { product: 'wings', quantity: 15},
-    ],
-    user: { loggedIn: true}
-}
+  cart: [
+    { product: 'pizza', quantity: 5 },
+    { product: 'wings', quantity: 15 },
+  ],
+  user: { loggedIn: true },
+};
 
 const stateClone = Object.assign({}, state);
 const stateCloneDeep = cloneDeep(state);
@@ -58,6 +67,25 @@ console.log(stateCloneDeep);
 state.user.loggedIn = false;
 console.log(stateClone);
 
+//Hot Module Replacement
+//Will not be added in the code
 
+if (module.hot) {
+  module.hot.accept();
+}
 
+class Person {
+  #greeting = 'Hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.#greeting} ${this.name}`);
+  }
+}
 
+const greeting = new Person('Hassaan');
+
+console.log(cart.filter(i => i.pieces <= 3));
+
+import 'core-js';
+
+import 'regenerator-runtime/runtime';
